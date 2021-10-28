645b0467735c1725ef8c581d66707ea905d90682
amolchaudhari1996 <91192554+amolchaudhari1996@users.noreply.github.com>
2021-10-28 10:30:52 +0530

    Delete Inheritance.java

--- src/inheritance/example/animal/Inheritance.java
+++
@@ -1,9 +0,0 @@
-package inheritance.example.animal;
-
-public class Inheritance {
-    public static void main(String args[]){
-        Animal dog = new Dog();
-        dog.setId(123);
-        dog.sound();
-    }
-}
